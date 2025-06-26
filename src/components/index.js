8b5e73e9f6cfce25a2e4b9a371563ca9717bbfe1
DubeyArpita <arpitadubey23216@gmail.com>
2025-06-26 23:00:30 +0530

    slug

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ import Footer from "./footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "../components/header/LogoutBtn";
-
-export { Header, Footer, Container, Logo, LogoutBtn };
+import RTE from "./RTE";
+export { Header, Footer, Container, Logo, LogoutBtn, RTE };
